284d64bfaa4f9e12047e49c79fb9d20f1365c467
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-08-29 22:12:01 -0500

    finished queries, saved change to data, added readme

--- queries.sql
+++ queries.sql
@@ -34,6 +34,12 @@ AND Album.Title = "A Night at the Opera"
 -- SELECT a.Title, s.Title FROM Album a LEFT JOIN Song s ON s.AlbumId = a.AlbumId;
 -- SELECT a.Title, s.Title FROM Song s LEFT JOIN Album a ON s.AlbumId = a.AlbumId;
 
+-- SELECT s.Title AS Song, al.Title AS Album, ar.ArtistName AS Artist
+-- FROM Song s
+-- LEFT JOIN Album al ON s.AlbumId = al.AlbumId
+-- LEFT JOIN Artist ar ON s.ArtistId = ar.ArtistId
+-- WHERE ar.ArtistName = 'Black Flag'
+
 SELECT al.Title AS 'album', s.Title AS 'song', ar.ArtistName AS 'artist'
 FROM  Song s 
 JOIN Artist ar ON ar.ArtistId = s.ArtistId
@@ -50,18 +56,35 @@ ORDER BY COUNT(songid) DESC
 
 -- 7. Write a SELECT statement to display how many songs exist for each artist. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
 
-SELECT COUNT(artistId)
-FROM Song
-WHERE artistId = 8;
+SELECT a.ArtistName, COUNT(songid) 'Total Songs'
+FROM song s
+LEFT JOIN Artist a ON s.ArtistId = a.ArtistId
+GROUP BY s.ArtistId
+ORDER BY COUNT(songid) DESC
 
 -- 8. Write a SELECT statement to display how many songs exist for each genre. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
 
+SELECT g.Label, COUNT(songid) 'Total Songs'
+FROM song s
+LEFT JOIN Genre g ON s.GenreID = g.GenreId
+GROUP BY s.GenreId
+ORDER BY COUNT(songid) DESC
+
 -- 9. Using MAX() function, write a select statement to find the album with the longest duration. The result should display the album title and the duration.
 
-SELECT AlbumLength, title
+SELECT AlbumLength, Title
 FROM album
 WHERE AlbumLength = (SELECT MAX(AlbumLength) FROM album)
 
 -- 10. Using MAX() function, write a select statement to find the song with the longest duration. The result should display the song title and the duration.
 
+SELECT SongLength, Title
+FROM Song
+WHERE SongLength = (SELECT MAX(SongLength) FROM Song)
+
 -- 11. Modify the previous query to also display the title of the album.
+
+SELECT s.SongLength, s.Title AS 'Song', a.Title AS 'Album'
+FROM Song s 
+JOIN Album a ON s.AlbumId = a.AlbumId
+WHERE SongLength = (SELECT MAX(SongLength) FROM Song)
